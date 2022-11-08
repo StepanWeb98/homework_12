a558ff6d4a22a25e16ee459c191f932e6f5d316c
stepa <stepanweb98@gmail.com>
2022-11-08 23:14:25 +0800

    version 3 changes

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 import json
+from json import JSONDecodeError
 from pprint import pprint
+import logging
 
 def load_posts(file):
     posts = []
@@ -9,14 +11,34 @@ def load_posts(file):
 
 # функция поиска введенного слова ?s="слово"
 def suche_post(key_wort):
+    try:
+        posts = load_posts("posts.json")
+        list_suche = []
+        for post in posts:
+            if key_wort in post['content']:
+                list_suche.append(post)
+        return list_suche
+    except FileNotFoundError:
+        # Будет выполнено, если файл не найден
+        print("Файл не найден")
+
+
+def save_picture(picture):
+    filename = picture.filename
+    file_type = filename.split(".")[-1]
+
+    if file_type in ["jpeg", "svg", "jpg", "png"]:
+        picture.save(f"./uploads/images/{filename}")
+        return f"uploads/images/{filename}"
+    else:
+        return False
+
+
+def save_post(posts, file="posts.json"):
+    with open(file, "w", encoding="utf-8") as write_file:
+        json.dump(posts, write_file)
+
+def add_post(post):
     posts = load_posts("posts.json")
-    list_suche = []
-    for post in posts:
-        if key_wort in post['content']:
-            list_suche.append(post)
-    return list_suche
-
-
-# def addition_post(file):
-#     with open(file, "w", encoding="utf-8") as write_file:
-
+    posts.append(post)
+    save_post(posts)
--- loader/views.py
+++ loader/views.py
@@ -3,18 +3,31 @@
 # Сперва импорттируем класс блюпринта
 from flask import render_template, Blueprint, request
 from functions import *
+import logging
+
 
 # Затем создаем новый блюпринт, выбираем для него имя
 loader_blueprint = Blueprint('loader_blueprint', __name__, template_folder="templates")
+logging.basicConfig(filename="basic.log", level=logging.INFO)
 
 # Создаем вьюшку, страницы поста, подключаем шаблон
-@loader_blueprint.route("/post", methods=["GET", "POST"])
+@loader_blueprint.route("/post")
 def profile_page():
     return render_template("post_form.html")
 
 # Создаем вьюшку, используя в декораторе блюпринт вместо app
-@loader_blueprint.route('/search')
+@loader_blueprint.route('/post', methods=["POST"])
 def category_page():
-    suche = request.args.get('s')
-    result_suche = suche_post(suche)
-    return render_template("post_list.html", suche=suche, result_suche=result_suche)
+    picture = request.files.get('picture')
+    content = request.form.get('content')
+
+    picture_url = save_picture(picture)
+
+    if picture_url is False:
+        logging.info(f"{picture_url} не изображение")
+        return "Не то изображение"
+
+    add_post({"pic": picture_url, "content": content})
+
+
+    return render_template("post_uploaded.html", picture_url=picture_url, content=content)
